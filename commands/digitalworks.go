6fe10299800f4ccc4aed70d170811cacc3fbf2bf
Jake Gealer <jake@gealer.email>
2020-11-08 13:20:32 +0000

    add container fetching support

--- commands/digitalworks.go
+++ commands/digitalworks.go
@@ -4,12 +4,12 @@ import (
 	"CSBot/categories"
 	"CSBot/router"
 	"context"
+	"errors"
 	"fmt"
 	"github.com/andersfylling/disgord"
 	"github.com/auttaja/gommand"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
@@ -17,6 +17,8 @@ import (
 	"math/rand"
 	"net"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func init() {
@@ -60,17 +62,46 @@ func init() {
 
 			// Check if the container already exists. If so manage showing the user options relating to this.
 			containerName := ctx.Message.Author.ID.String() + "-vnc"
-			filter := filters.NewArgs()
-			filter.ExactMatch("name", containerName)
-			containers, err := cli.ContainerList(context.TODO(), types.ContainerListOptions{Filters: filter})
-			if err != nil {
-				return err
-			}
-			if len(containers) != 0 {
+			c, err := cli.ContainerInspect(context.TODO(), containerName)
+			if err == nil {
 				message := "You already have a DigitalWorks container running. This means you have 2 options:\n\n" +
 					"♻️ **Destroy the container:** You will want to do this if you want to change the resolution of your container. Note that this will not destroy your persistent folder on your desktop, but will destroy all other container content.\n" +
 					"✉️ **Re-send the credentials:** Resends the login credentials in a DM."
-				_, _ = ctx.Reply(message)
+				msg, err := ctx.Reply(message)
+				if err == nil {
+					deadline, _ := context.WithTimeout(context.TODO(), time.Minute*10)
+					r := ctx.WaitManager.WaitForMessageReactionAdd(deadline, func(_ disgord.Session, evt *disgord.MessageReactionAdd) bool {
+						return evt.MessageID == msg.ID && evt.UserID == ctx.Message.Author.ID && (evt.PartialEmoji.String() == "♻" || evt.PartialEmoji.String() == "✉️")
+					})
+					_ = ctx.Session.DeleteMessage(context.TODO(), msg.ChannelID, msg.ID)
+					if r != nil {
+						if r.PartialEmoji.String() == "♻" {
+							// Destroy the container.
+							_ = cli.ContainerRemove(context.TODO(), c.ID, types.ContainerRemoveOptions{Force: true})
+							_, _ = ctx.Reply(ctx.Message.Author.Mention(), "Database deleted.")
+						} else {
+							// Resend the credentials via DMs.
+							conn, err := net.Dial("udp", "8.8.8.8:80")
+							if err != nil {
+								log.Fatal(err)
+							}
+							defer conn.Close()
+							remoteAddr := conn.LocalAddr().(*net.UDPAddr).IP.String()
+							var password string
+							for _, v := range c.Config.Env {
+								if strings.HasPrefix(v, "VNC_PASSWORD=") {
+									password = v[13:]
+									break
+								}
+							}
+							if password == "" {
+								return errors.New("password field is blank for some weird reason")
+							}
+							port := c.HostConfig.PortBindings["5900/tcp"][0].HostPort
+							_, _, _ = ctx.Message.Author.SendMsg(context.TODO(), ctx.Session, &disgord.Message{Content: "Hostname: "+remoteAddr+":"+port+"\nPassword: "+password})
+						}
+					}
+				}
 				return nil
 			}
 
@@ -129,10 +160,6 @@ func init() {
 			defer conn.Close()
 			remoteAddr := conn.LocalAddr().(*net.UDPAddr).IP.String()
 			_, _, _ = ctx.Message.Author.SendMsg(context.TODO(), ctx.Session, &disgord.Message{Content: "Hostname: "+remoteAddr+":"+port+"\nPassword: "+password})
-
-			// Garbage collect.
-			//_ = cli.ContainerRemove(context.TODO(), res.ID, types.ContainerRemoveOptions{Force: true})
-			//_, _ = ctx.Reply(ctx.Message.Author.Mention(), "Database deleted.")
 
 			// Return no errors.
 			return nil
